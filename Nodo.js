262aa672e17064de67e2efa75369b3de775654cb
Karen Vazquez <mitchelvazquez348@gmail.com>
2024-10-17 10:46:02 -0600

    Listas v 1.0.2

---
+++ Nodo.js
@@ -0,0 +1,35 @@
+Nodo
+ -dato
+ -next( referencia al siguiente nodo)
+
+
+ let nodo1 = new Nodo(1)
+ let nodo2 = new Nodo(2)
+ let nodo3 = new Nodo(3)
+
+
+listaEnlazada = new ListaEnlazada()
+
+dato,   next
+5,      -> 99
+99 - >  7
+7,     -> 8
+8,      ->40
+40,     null
+ 
+
+
+
+
+
+listaEnlazada.removeData(40)
+
+
+
+
+
+
+ 
+
+
+
